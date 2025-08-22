dff2f2c9fcaadda87277ebc4ce749f7e340d38d4
Clare MacAdie <clare@macadie.co.uk>
2025-08-22 23:51:22 +0100

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,12 +1,22 @@
+class TimeoutError extends Error {
+  constructor(message = 'Operation timed out') {
+    super(message);
+    this.name = 'TimeoutError';
+  }
+}
+
 class App {
   constructor(url) {
     this.url = url;
     this.schedules = [];
     this.schedulesTally = {};
+
+    this.init();
+    this.bind();
   }
 
   setTimeoutPromise(delay) {
-    return new Promise((_, reject) => setTimeout(() => reject('Request timed out. Please try again.'), delay));
+    return new Promise((_, reject) => setTimeout(() => reject(new TimeoutError()), delay));
   }
 
   async fetchSchedules(path) {
@@ -26,7 +36,7 @@ class App {
 
   displaySchedules() {
     if (this.schedules.length === 0) {
-      alert('No schedules are available for booking');
+      alert('There are currently no schedules available for booking');
     } else {
       this.createSchedulesTally();
       let msg = '';
@@ -35,6 +45,19 @@ class App {
       }
       alert(msg);
     }
+  }
+
+  async sendFetch(data) {
+    let formPath = this.$staffForm.getAttribute('action');
+    let formMethod = this.$staffForm.method;
+
+    return await fetch(this.url + formPath, {
+        'method': formMethod,
+        'body': data,
+        'headers': {
+          'Content-Type': 'application/json',
+        }
+      });
   }
 
   createSchedulesTally() {
@@ -47,12 +70,48 @@ class App {
       }
     });
   }
+
+  async createNewStaff(inputEmail, inputName) {
+    let data = JSON.stringify({'email': inputEmail, 'name': inputName});
+
+    try {
+      let response = await this.sendFetch(data);
+
+      if (!response.ok) throw new Error('Response not ok');
+      if(response.status >= 400 && response.status < 500) throw new Error('Staff can not be created. Check your inputs.');
+
+      let id = (await response.json()).id;
+      alert(`Successfully created staff with id: ${id}`);
+    } catch(error) {
+      alert(error);
+    }
+  }
+
+  async handleStaffFormSubmit(event) {
+    event.preventDefault();
+
+    let inputEmail = this.$staffForm.email.value.trim();
+    let inputName = this.$staffForm.name.value.trim();
+    if (!inputEmail || !inputName) {
+      alert('Staff cannot be created. Check your inputs.');
+    } else {
+      let id = await this.createNewStaff(inputEmail, inputName);
+    }
+  }
+
+  init() {
+    this.$staffForm = document.getElementById('staff-form');
+  }
+
+  bind() {
+    this.$staffForm.addEventListener('submit', this.handleStaffFormSubmit.bind(this));
+  }
 }
 
 async function main() {
-  let url = 'http://localhost:3000/';
+  let url = 'http://localhost:3000';
   let app = new App(url);
-  await app.fetchSchedules('api/schedules');
+  // await app.fetchSchedules('/api/schedules');
 }
 
 document.addEventListener('DOMContentLoaded', main);
