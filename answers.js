51e7ffbfb4dafaa444689c44f386879e6af89475
Clare MacAdie <clare@macadie.co.uk>
2025-08-22 23:53:19 +0100

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,10 +1,3 @@
-class TimeoutError extends Error {
-  constructor(message = 'Operation timed out') {
-    super(message);
-    this.name = 'TimeoutError';
-  }
-}
-
 class App {
   constructor(url) {
     this.url = url;
@@ -13,38 +6,6 @@ class App {
 
     this.init();
     this.bind();
-  }
-
-  setTimeoutPromise(delay) {
-    return new Promise((_, reject) => setTimeout(() => reject(new TimeoutError()), delay));
-  }
-
-  async fetchSchedules(path) {
-    try {
-      let response = await Promise.race([fetch(this.url + path), this.setTimeoutPromise(5000)]);
-
-      if (!response.ok) throw new Error('Reponse not ok');
-      this.schedules = await response.json();
-      this.displaySchedules();
-      alert('Request completed successfully.');
-
-    } catch (error) {
-      alert(error);
-      alert('Request failed.');
-    }
-  }
-
-  displaySchedules() {
-    if (this.schedules.length === 0) {
-      alert('There are currently no schedules available for booking');
-    } else {
-      this.createSchedulesTally();
-      let msg = '';
-      for (let staffId in this.schedulesTally) {
-        msg += `${staffId}: ${this.schedulesTally[staffId]}\n`;
-      }
-      alert(msg);
-    }
   }
 
   async sendFetch(data) {
@@ -58,17 +19,6 @@ class App {
           'Content-Type': 'application/json',
         }
       });
-  }
-
-  createSchedulesTally() {
-    this.schedules.forEach(schedule => {
-      let staffId = `Staff ${schedule.staff_id}`;
-      if (this.schedulesTally[staffId]) {
-        this.schedulesTally[staffId] += 1;
-      } else {
-        this.schedulesTally[staffId] = 1;
-      }
-    });
   }
 
   async createNewStaff(inputEmail, inputName) {
@@ -111,7 +61,6 @@ class App {
 async function main() {
   let url = 'http://localhost:3000';
   let app = new App(url);
-  // await app.fetchSchedules('/api/schedules');
 }
 
 document.addEventListener('DOMContentLoaded', main);
